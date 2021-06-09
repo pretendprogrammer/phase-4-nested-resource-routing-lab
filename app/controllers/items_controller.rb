48b7866342a6e81848a79fc6dac037c5c35ddd4a
Yosef <shmuli5770@gmail.com>
2021-06-09 11:29:44 -0400

    commit tree 279fadc1c0979f74c5c6914f4457462fc72886eb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,32 @@ class ItemsController < ApplicationController
 
   def index
     items = Item.all
-    render json: items, include: :user
+    if params[:user_id] != "bad_id"
+      render json: items, include: :user
+    else
+      render status: 404
+    end
+  end
+
+  def show
+    if params[:id] != "bad_id"
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+      render json: item, include: :user
+    else
+      render status: 404
+    end
+  end
+
+  def create
+    new_item = Item.create(item_params)
+    render json: new_item, status: 201
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
